5da6be96a3d7a54c01a330a83d34ca4066406f60
张三丰 <123456@qq.com>
2017-02-28 22:04:38 +0800

    最简单的webpack.config.js配置

---
+++ webpack-demo/src/script/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sf on 2017/2/28.
+ */
+
+function helloworld() {
+    console.log("hello world");
+}
+
+helloworld();
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,3 +1,11 @@
 /**
  * Created by sf on 2017/2/28.
  */
+
+module.exports = {
+    entry: './src/script/main.js',      //入口文件
+    output: {
+        path: './dist/js',              //输入的文件夹
+        filename: 'bundle.js'           //输出的文件名
+    }
+};
