97dd5fc5f9e5b036f7e891197bdd451a3a015f3a
张三丰 <123456@qq.com>
2017-03-03 13:28:24 +0800

    两个页面，引入相同的JS

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -15,6 +15,11 @@ module.exports = {
             template: 'index.html',     //页面输入的模板
             filename: 'index.html',     //输出的文件名
             inject: 'head',             //head中插入打包的js
+        }),
+        new htmlWebpackPlugin({
+            template: 'index2.html',     //页面输入的模板
+            filename: 'index2.html',     //输出的文件名
+            inject: 'head',             //head中插入打包的js
         })
     ]
 };
