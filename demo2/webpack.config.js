65941e30d4ba500c14d307fa1c13678b45779cfe
张三丰 <123456@qq.com>
2017-03-03 13:56:34 +0800

    多个页面实现中引入不出的JS（1）

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -4,32 +4,56 @@
 
 var htmlWebpackPlugin = require('html-webpack-plugin');
 
+// module.exports = {
+//     //entry: './src/script/main.js',      //打包的输入文件
+//     //entry: ['./src/script/main.js', './src/script/main2.js'],
+//     // output: {
+//     //     path: './dist',                  //要输出的目录
+//     //     filename: 'main.bundle.js'       //要输出的名称
+//     // },
+//     entry: {
+//         main: './src/script/main.js',
+//         main2: './src/script/main2.js'
+//     },
+//     output: {
+//         path: './dist',
+//         //filename: '[name].js'
+//         filename: '[chunkhash].js'
+//     },
+//     plugins: [
+//         new htmlWebpackPlugin({
+//             template: 'index.html',     //页面输入的模板
+//             filename: 'index.html',     //输出的文件名
+//             inject: 'head',             //head中插入打包的js
+//         }),
+//         new htmlWebpackPlugin({
+//             template: 'index2.html',
+//             filename: 'index2.html',
+//             inject: 'head'
+//         })
+//     ]
+// };
+
 module.exports = {
-    //entry: './src/script/main.js',      //打包的输入文件
-    //entry: ['./src/script/main.js', './src/script/main2.js'],
-    // output: {
-    //     path: './dist',                  //要输出的目录
-    //     filename: 'main.bundle.js'       //要输出的名称
-    // },
     entry: {
         main: './src/script/main.js',
         main2: './src/script/main2.js'
     },
     output: {
         path: './dist',
-        //filename: '[name].js'
         filename: '[chunkhash].js'
     },
     plugins: [
         new htmlWebpackPlugin({
-            template: 'index.html',     //页面输入的模板
-            filename: 'index.html',     //输出的文件名
-            inject: 'head',             //head中插入打包的js
+            template: 'indexx.html',
+            filename: 'index.html',
+            //inject: 'head'
+            inject: false
         }),
         new htmlWebpackPlugin({
             template: 'index2.html',
             filename: 'index2.html',
-            inject: 'head'
+            inject: false
         })
     ]
 };
