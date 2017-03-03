209e2509966f9b5f1b1f3097d1a7e7cdd7016c5d
张三丰 <123456@qq.com>
2017-03-03 11:39:46 +0800

    安装webpack

---
+++ demo2/webpack.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sf on 2017/3/3.
+ */
+
+module.exports = {
+    entry: './src/script/main.js',      //打包的输入文件
+    output: {
+        path:'./dist',                  //要输出的目录
+        filename:'main.bundle.js'       //要输出的名称
+    }
+};
